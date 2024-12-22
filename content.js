f5e90639ccd81940754fa8e3a9e8ef48f7f2c01b
Mortefix <Dwagner@tutanota.com>
2024-12-22 16:15:47 -0500

    Update content.js

--- content.js
+++ content.js
@@ -1,13 +1,11 @@
-// Function to remove the ad and upgrade div
 function removeAdAndUpgrade() {
-  // Remove the upgrade container
+
   const upgradeDiv = document.querySelector("div.css-1euatyy-ShinyUpgradeContainer");
   if (upgradeDiv) {
     upgradeDiv.remove();
     console.log("Upgrade div removed.");
   }
 
-  // Remove the ad div
   const adDiv = document.querySelector("div#ads-table");
   if (adDiv) {
     adDiv.remove();
@@ -15,13 +13,10 @@ function removeAdAndUpgrade() {
   }
 }
 
-// Create a MutationObserver to watch for changes in the DOM
 const observer = new MutationObserver(() => {
   removeAdAndUpgrade();
 });
 
-// Observe changes to the body of the document
 observer.observe(document.body, { childList: true, subtree: true });
 
-// Run the function on initial load in case the elements are already present
 removeAdAndUpgrade();
